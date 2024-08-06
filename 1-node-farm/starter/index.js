6754f40e985b4b4e736d94479413063757944294
choiys99 <cys1849908@gmail.com>
2024-08-06 20:08:19 +0900

    node js 32

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,62 +1,98 @@
-const fs = require("fs");
-const http = require("http");
-const url = require("url");
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./module/replaceTemplate');
+// const slugify = require("slugify");
+// const replaceTemplate = require("./modules/replaceTemplate");
 // node 외부 경로 받을 수 있음 require
 
 //////////////////
 // file
 
 // 차단,동기장식
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8"); // 파일읽기의 동기기버전 (경로/인코딩된문자)
-// console.log(textIn);
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8"); // 파일읽기의 동기기버전 (경로/인코딩된문자)
+// // console.log(textIn);
 
-const textOut = `This is what we know about the avocado : ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut); // text 생성
-// console.log("File written!");
+// const textOut = `This is what we know about the avocado : ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut); // text 생성
+// // console.log("File written!");
 
-// 비차단,비동기
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  if (err) return console.log("1번 에러!!");
+// // 비차단,비동기
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("1번 에러!!");
 
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    if (err) return console.log("2번 에러!!");
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     if (err) return console.log("2번 에러!!");
 
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      if (err) return console.log("3번 에러!!");
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       if (err) return console.log("3번 에러!!");
 
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("파일이 작성되었습니다.");
-      });
-    });
-  });
-});
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         // console.log("파일이 작성되었습니다.");
+//       });
+//     });
+//   });
+// });
 // console.log("will read file");
 
 //////////////////
 // server
 
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+
+const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+// console.log(data);
+
 const server = http.createServer((req, res) => {
   // 요청이 서버에 도달할 때마다 실행되는 콜백함수를 허용
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
+  //(요청한 url경로,true = 쿼리문자열을 자동으로 객체 ,false 또는 생략하면 쿼리문자열을 문자열형태로)
 
-  if (pathName === "/" || pathName === "/overview") {
-    res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+  // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
 
-    res.end("overview 입니다");
-  } else if (pathName === "/product") {
-    res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+    // Product page
+  } else if (pathname === '/product') {
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
 
-    res.end("product 입니다");
+    res.end(output);
+
+    // API
+  } else if (pathname === '/api') {
+    res.end(data);
+
+    // NOT found
   } else {
-    res.writeHead(404, { "Content-Type": "text/html; charset=utf-8" });
-    res.end("<h1>찾을 수 없는 페이지입니다.</h1>");
+    res.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.end('<h1>찾을 수 없는 페이지입니다.</h1>');
   }
 });
 
-server.listen(8000, "127.0.0.1", () => {
+server.listen(8000, '127.0.0.1', () => {
   // listen = 수신대기)  .. > 8000은 서버가 수신할 포트 번호
-  console.log("Listening to requests on port 8000");
+  console.log('Listening to requests on port 8000');
   // http://127.0.0.1:8000
 });
-
-test;
---
+++ 1-node-farm/starter/module/replaceTemplate.js
@@ -0,0 +1,17 @@
+module.exports = (temp, product) => {
+  // / = 정규표현식
+  // g = 글로벌이다 => 해당 문자열이 여러번 등장하면 다 찾겟다.
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+  return output;
+};
