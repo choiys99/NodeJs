5c0a0bba29778376dca17fcb5492608393ad3195
choiys99 <cys1849908@gmail.com>
2024-08-10 16:42:19 +0900

    node js post requests

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,15 +1,28 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  // get 요청시 message 실행
-  res.status(200).json({ message: '첫 번째 Get 요청', app: 'Natours' });
-  //{} = 로 json형식의 응답
+app.use(express.json());
+//Express 애플리케이션에서 JSON 형태의 요청(request) body를 파싱(parse)하기 위해 사용되는 미들웨어
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: '썽꽁',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('첫번째 post 요청');
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.send('DDDDD');
 });
 
 const port = 3000;
